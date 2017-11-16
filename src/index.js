f23226f7cd99705256e249eda09478beeb919c82
Shymakher <shymakher@gmail.com>
2017-11-16 14:03:44 +0200

    1. Created main  component post_index 2. Added an action creator as fetchPosts 3. Created reducer_posts

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {BrowserRouter, Route} from 'react-router-dom';
+import PostIndex from './components/post_index';
 import reducers from './reducers';
+import ReduxPromise from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <Route path="/" component={PostIndex}/>
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.querySelector('.container'));
